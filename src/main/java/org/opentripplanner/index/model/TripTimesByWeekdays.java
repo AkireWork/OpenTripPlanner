c4ea2da8d8b4186461749dc76201a5598df85e92
Martin <martintonts@gmail.com>
2021-02-25 14:27:35 +0200

    REIS-132 second part

--- src/main/java/org/opentripplanner/index/model/TripTimesByWeekdays.java
+++ src/main/java/org/opentripplanner/index/model/TripTimesByWeekdays.java
@@ -6,9 +6,24 @@ import java.util.List;
 
 public class TripTimesByWeekdays {
     public String weekdays;
+    public int arrival;
     public List<TripTimeShort> tripTimeShortList = Lists.newArrayList();
 
     public TripTimesByWeekdays(String weekday) {
         this.weekdays = weekday;
+    }
+
+    public TripTimesByWeekdays(String weekdays, List<TripTimeShort> list) {
+        this.weekdays = weekdays;
+        this.tripTimeShortList = list;
+    }
+
+    @Override
+    public String toString() {
+        return "TripTimesByWeekdays{" +
+                "weekdays='" + weekdays + '\'' +
+                ", arrival=" + arrival +
+                ", tripTimeShortList=" + tripTimeShortList +
+                '}';
     }
 }
